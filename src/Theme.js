7236c02c772e45782523874e91193662b11a9a02
Derek Yu <derekyu624@hotmail.com>
2019-09-01 18:38:25 -0400

    add education section to about me

--- src/Theme.js
+++ src/Theme.js
@@ -6,7 +6,7 @@ export const theme = createMuiTheme({
             main: '#179fd4',
         },
         secondary: {
-            main: '#ffffff',
+            main: '#f73378',
         },
     },
     typography: {
--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -1,45 +1,93 @@
 import React from 'react';
-import LazyLoad from 'react-lazy-load';
+import { Card, CardContent, CardMedia, Grid } from '@material-ui/core';
 import { ImageLoader } from '../common/ImageLoader';
 
 import './AboutMe.css';
 
+const education = [
+    {
+        school: 'Concordia University',
+        program: 'Software Engineering (BEng.)',
+        period: '2016-2020 (Expected)',
+        img: require('../../resources/concordia-logo.jpg'),
+        url: 'https://www.concordia.ca/',
+    },
+    {
+        school: 'Marianopolis College',
+        program: 'Pure & Applied Sciences (DEC)',
+        period: '2014-2016',
+        img: require('../../resources/Marianopolis.png'),
+        url: 'http://www.marianopolis.edu/',
+    },
+];
+
 export class AboutMe extends React.Component {
+    listEducation = () =>
+        education.map((x) => (
+            <Grid item xs={6}>
+                <Card
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <a href={x.url} target='_blank' rel='noopener noreferrer'>
+                        <CardMedia
+                            style={{ height: '70px', width: '70px' }}
+                            image={x.img}
+                        />
+                    </a>
+                    <CardContent>
+                        <h5>{x.school}</h5>
+                        <div className='education-subcontents'>
+                            {x.program}
+                            <br />
+                            <em>{x.period}</em>
+                        </div>
+                    </CardContent>
+                </Card>
+            </Grid>
+        ));
+
     render() {
         return (
             <div className='about-me-container' id='about-me'>
                 <h1>ABOUT ME</h1>
-                <div className='underline'></div>
+                <div className='underline' />
                 <div className='content'>
                     <div className='profile-picture-container'>
-                        <LazyLoad debounce={false} offsetVertical={500}>
-                            <ImageLoader
-                                style={{ height: '70vh' }}
-                                src={require('../../resources/DSC_0376.jpg')}
-                            />
-                        </LazyLoad>
+                        <ImageLoader
+                            style={{ height: '70vh' }}
+                            src={require('../../resources/DSC_0376.jpg')}
+                        />
                     </div>
 
                     <div className='description'>
                         <p>
-                            I'm a fourth-year software engineering student at 
-                            Concordia University in Montreal, Canada. I first 
-                            got into programming when I joined the robotics team at
-                            school and instantly fell in love with all the creative 
-                            possibilities that came with coding.
+                            I'm a fourth-year software engineering student at
+                            Concordia University in Montreal, Canada. I first
+                            got into programming when I joined the robotics team
+                            at school and instantly fell in love with all the
+                            creative possibilities that came with coding.
                         </p>
                         <p>
-                            I find myself drawn to backend development as I'm most 
-                            intrigued by the architectural and algorithmic challenges
-                            that come with it. I have experience developing in several 
-                            languages and frameworks (such as TypeScript, Java and Python 
-                            to name a few).
+                            I find myself drawn to backend development as I'm
+                            most intrigued by the architectural and algorithmic
+                            challenges that come with it. I have experience
+                            developing in several languages and frameworks (such
+                            as TypeScript, Java and Python to name a few).
                         </p>
                         <p>
-                            Outside of my life of software development, I also love doing
-                            photography, travelling & exploring, eating, and following
-                            the latest tech news.
+                            Outside of my life of software development, I also
+                            love doing photography, travelling & exploring,
+                            eating, and following the latest tech news.
                         </p>
+                        <h3>Education</h3>
+                        <Grid container spacing={3}>
+                            {this.listEducation()}
+                        </Grid>
                     </div>
                 </div>
             </div>
