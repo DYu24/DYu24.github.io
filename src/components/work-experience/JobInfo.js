70de65cae90e0bbb89fb4f0686b020c287e05b2e
Derek Yu <derekyu624@hotmail.com>
2019-09-01 02:16:32 -0400

    minor stylistic changes

--- src/components/work-experience/JobInfo.js
+++ src/components/work-experience/JobInfo.js
@@ -11,18 +11,21 @@ export class JobInfo extends React.Component {
             <div className='job-info-container'>
                 <div className='job-info-content'>
                     <h2>
-                        <a href={url} target='_blank' rel='noopener noreferrer'>{company}</a>
+                        <a href={url} target='_blank' rel='noopener noreferrer'>
+                            {company}
+                        </a>
                     </h2>
-                    <h3>
-                        {position} ({period})
-                    </h3>
+                    <h3>{position}</h3>
+                    <h5>{period}</h5>
                     <p>{description}</p>
                 </div>
                 <div className='job-info-visual'>
-                    <ImageLoader
-                        style={{ maxHeight: '200px', maxWidth: '200px' }}
-                        src={src}
-                    />
+                    <a href={url} target='_blank' rel='noopener noreferrer'>
+                        <ImageLoader
+                            style={{ maxHeight: '200px', maxWidth: '200px' }}
+                            src={src}
+                        />
+                    </a>
                 </div>
             </div>
         );
