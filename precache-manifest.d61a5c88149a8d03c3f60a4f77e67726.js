eb0daafccccb6bdd302d8f3c40152d226f79289d
Derek Yu <derekyu624@hotmail.com>
2019-09-02 01:37:21 -0400

    Updates

---
+++ precache-manifest.d61a5c88149a8d03c3f60a4f77e67726.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a48a6fdc39dae96ec9d01ca39b1baa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf644e68ac1db34bf19a",
+    "url": "/static/css/main.54127994.chunk.css"
+  },
+  {
+    "revision": "8f0fb3b2d5beff8a125b",
+    "url": "/static/js/2.ed8840cb.chunk.js"
+  },
+  {
+    "revision": "cf644e68ac1db34bf19a",
+    "url": "/static/js/main.bf25af61.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "73a6f1b61b1c81f7ba3bdc1d00ea6b38",
+    "url": "/static/media/DSC_0376.73a6f1b6.jpg"
+  },
+  {
+    "revision": "f25c1417e69295cfba23e93667a3cbbb",
+    "url": "/static/media/Marianopolis.f25c1417.png"
+  },
+  {
+    "revision": "48dbff7bc9f29e60df6041b79640ef46",
+    "url": "/static/media/TapOfWarLogo.48dbff7b.png"
+  },
+  {
+    "revision": "c76eeacf17bf12c57aae2f35c7b7c53a",
+    "url": "/static/media/cae-logo.c76eeacf.png"
+  },
+  {
+    "revision": "d659daba724aba890f646c15ccb92cd9",
+    "url": "/static/media/caption-this-logo.d659daba.png"
+  },
+  {
+    "revision": "ce637f052c96429842156e55a1e4c815",
+    "url": "/static/media/chefpic-logo.ce637f05.png"
+  },
+  {
+    "revision": "47862c9add946a86267184b99037f03d",
+    "url": "/static/media/concordia-logo.47862c9a.jpg"
+  },
+  {
+    "revision": "6efac0ae5bf7ebb6adb1552864478c74",
+    "url": "/static/media/cv-icon.6efac0ae.png"
+  },
+  {
+    "revision": "c63e99782c1666c62a1bfa540fbaaafd",
+    "url": "/static/media/dott-logo.c63e9978.png"
+  },
+  {
+    "revision": "b7b9d7b8d8a50f93c6f6374b3e6951df",
+    "url": "/static/media/linkedin-logo.b7b9d7b8.png"
+  },
+  {
+    "revision": "d9da63d4769e2035d0d00dfdef459c2a",
+    "url": "/static/media/resume.d9da63d4.pdf"
+  },
+  {
+    "revision": "c0f0a3aa98efd27d5e22c69261a8a5af",
+    "url": "/static/media/rudating.c0f0a3aa.png"
+  },
+  {
+    "revision": "f6b3c559f53bacd64c0c3ed76eae9e7c",
+    "url": "/static/media/sqm-logo.f6b3c559.png"
+  }
+]);
