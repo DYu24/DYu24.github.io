b231a53c18b85a5371e49a7fc945dca46a371bf4
Derek Yu <derekyu624@hotmail.com>
2020-08-24 23:47:32 -0400

    Updates

--- precache-manifest.255d39eefa286e00b813d5d282bb93d8.js
+++ precache-manifest.255d39eefa286e00b813d5d282bb93d8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc98785cbb4e2a09a14872af17e95175",
+    "revision": "fa5ca792f4419befc4403b5e149d4440",
     "url": "/index.html"
   },
   {
-    "revision": "099a4a47b264ac176bdb",
+    "revision": "c8b1aa6fb524ef8a0953",
     "url": "/static/css/main.498d002a.chunk.css"
   },
   {
-    "revision": "83d5502619214d8d9099",
-    "url": "/static/js/2.7f07356e.chunk.js"
+    "revision": "4ad31cbbb932b7bcf2b5",
+    "url": "/static/js/2.e07fbf66.chunk.js"
   },
   {
     "revision": "7e402396ca428a5bd514c48df615b24e",
-    "url": "/static/js/2.7f07356e.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e07fbf66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "099a4a47b264ac176bdb",
-    "url": "/static/js/main.a26f647f.chunk.js"
+    "revision": "c8b1aa6fb524ef8a0953",
+    "url": "/static/js/main.da7b9b8f.chunk.js"
   },
   {
     "revision": "e187b7019aa5ad705895",
@@ -30,6 +30,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "48dbff7bc9f29e60df6041b79640ef46",
     "url": "/static/media/TapOfWarLogo.48dbff7b.png"
+  },
+  {
+    "revision": "ed6b5e833d842e309a03f2e040eb4f51",
+    "url": "/static/media/amazon-logo.ed6b5e83.png"
   },
   {
     "revision": "c76eeacf17bf12c57aae2f35c7b7c53a",
@@ -60,8 +64,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/linkedin-logo.b7b9d7b8.png"
   },
   {
-    "revision": "1ea8fa0fabd095f7128808745145d31a",
-    "url": "/static/media/resume.1ea8fa0f.pdf"
+    "revision": "cdd10c933625a8027afd323fa787b7b2",
+    "url": "/static/media/resume.cdd10c93.pdf"
   },
   {
     "revision": "9e5ffccc1aa055cb075268dca9d1044f",
